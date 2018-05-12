97edbee78be8f0014562241de096cb9a7b713892
polaaditya <polaaditya@gmail.com>
2018-05-12 11:46:04 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Welcome to My World!";
+    private final String message = "Welcome to My World Pipeline!";
 
     public App() {}
 
